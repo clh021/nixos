d5dc9f4fa3c852e9ea6735ce955fbba18d97f933
chenlianghong <clh021@gmail.com>
2021-10-23 09:02:16 +0800

    add i18n libime for libime_pinyindict

--- modules/i18n.nix
+++ modules/i18n.nix
@@ -6,7 +6,11 @@
     defaultLocale = "en_US.UTF-8";
     supportedLocales = [ "zh_CN.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
     inputMethod.enabled = "fcitx5";
-    inputMethod.fcitx5.addons  = [ pkgs.fcitx5-chinese-addons ];
+    inputMethod.fcitx5.addons  = [
+        pkgs.fcitx5-chinese-addons
+        # for libime_pinyindict
+        pkgs.libime
+    ];
   };
   console.keyMap = "us";
 }
